a895fa644ca37878608b9f8c7816774c7c124956
webmans-ru <lukoyanov_aa@webmens.ru>
2019-02-13 19:35:45 +0300

    Логика перенесена в родительский контроллер

--- controllers/AdminBaseController.php
+++ controllers/AdminBaseController.php
@@ -8,20 +8,5 @@ use \yii\web\HttpException;
 class AdminBaseController extends \app\modules\b24\controllers\AdminBaseController {
     
     public $layout = '@app/modules/forms/views/layouts/admin.php';
-
-public function actionInstall() {
-        $request = Yii::$app->request;
-        $issetPortal = $this->isSetB24portal($request->get('DOMAIN'));
-        if ($issetPortal) {
-            return $this->render('index');
-        } else {
-            return $this->render('install');
-        }
-    }
-
-    public function actionIndex() {
-        return $this->render('index');
-    }
-
 }
 
