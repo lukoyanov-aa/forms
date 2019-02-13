2ae62b2138880b3fc57a32a0fe3f9c76f7bd9de1
webmans-ru <lukoyanov_aa@webmens.ru>
2019-02-13 17:33:28 +0300

    Изменение индекса

--- views/admin-base/index.php
+++ views/admin-base/index.php
@@ -8,72 +8,18 @@ use yii\bootstrap\Tabs;
 /* @var $searchModel app\modules\b24\models\B24portalSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-echo Tabs::widget([
-    'items' => [
-        [
-            'label' => 'Yii2',
-            'content' => '<h2>Фреймворк Yii 2 - один из самых быстрых, безопасных и "крутых" php-фреймворков.</h2>',
-            'active' => true
-        ],
-        [
-            'label' => 'jQuery',
-            'content' => '<h2>jQuery - один из самых популярных JavaScript фреймворков, который работает с объектами DOM.</h2>'
-        ],
-        [
-            'label' => 'Bootstrap',
-            'content' => '<h2>Twitter Bootstrap - супер фреймворк, объединяющий в себе html, css, и JavaScript для для верстки веб-интерфейсов и страниц.</h2>',
-            'headerOptions' => [
-                'id' => 'headerOptions'
-            ],
-            'options' => [
-                'id' => 'options'
-            ]
-        ],
-        [
-            'label' => 'Еще табы',
-            'content' => '<h2>Вы можете добавить любое количество табов. Просто опишите их структуру в массиве.</h2>'
-        ],
-        [
-            'label' => 'Выпадающий список табов',
-            'items' => [
-                [
-                    'label' => 'Первый таб из выпадающего списка',
-                    'content' => '<h2>Обновите свои познания в Yii 2 and Twitter Bootstrap. Все возможнсти уже обернуты в удобные интерфейсы.</h2>'
-                ],
-                [
-                    'label' => 'Второй таб из выпадающего списка',
-                    'content' => '<h2>Один в поле не воин, а двое - уже компания.</h2>'
-                ],
-                [
-                    'label' => 'Это третий таб из выпадающего списка',
-                    'content' => '<h2>Третий не лишний!</h2>'
-                ]
-            ]
-        ]
-    ]
-]);
 
-$this->title = 'B24portals';
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = 'Формы';
 ?>
 <div class="b24portal-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
     
 </div>
-<div class="b24-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
+<div class="b24-default-index">    
     <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
-    
-    <p>
-        <?php //echo '<pre>' .print_r($arCurrentB24User, true). '<pre>'; ?>
+        Вы находитесь на главной странице приложения разработанного компанией Webmens.ru.<br> 
+        Все настройки осуществляются через пункты меню в верхней части приложения.<br>
+        В случая возникновения трудностей или обнаружения ошибок в работе приложения просьба сообщить нам на email: dev.webmens.ru
     </p>
 </div>
